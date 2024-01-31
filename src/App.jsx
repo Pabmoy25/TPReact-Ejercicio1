eea2fd5f4f7cb5f519dee1374922312f064d39ff
Pabmoy25 <pabmoy1725@gmail.com>
2024-01-31 13:59:34 -0300

    se agrega el componente Footer y se realiza su maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Saludo from './components/Saludo'
+import Footer from "./components/Footer";
 
 function App() {
   
-
   return (
     <>
     <section className='container mt-5 mainpage text-center'>
       <Saludo></Saludo>
     </section>
+
+    <Footer></Footer>
     </>
   )
 }
